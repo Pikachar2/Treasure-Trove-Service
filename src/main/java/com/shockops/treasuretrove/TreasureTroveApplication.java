59b28b27cd84d34386ae2f8c6389490d630cd095
Zach <highiq2@hotmail.com>
2018-08-12 11:52:45 -0400

    Basic Spring project

---
+++ src/main/java/com/shockops/treasuretrove/TreasureTroveApplication.java
@@ -0,0 +1,12 @@
+package com.shockops.treasuretrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureTroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasureTroveApplication.class, args);
+	}
+}
